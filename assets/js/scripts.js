3e73095e612cc3bd4f94bb8b62efdab7b7a9507c
Unknown <typict@ukr.net>
2018-05-07 11:58:58 +0300

    respons1

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -62,9 +62,9 @@ if (typeof jQuery === 'undefined') {
                         autoplaySpeed: 3000,
                       responsive: [
                           {
-                          breakpoint: 1600,
+                          breakpoint: 1300,
                           settings: {
-
+                            slidesToShow: 3
                           }
                         },
                         {
@@ -268,3 +268,16 @@ if (typeof jQuery === 'undefined') {
 
 $( ".tabs__caption li" ).first().addClass( "active" );
 $( ".tabs .tabs__content" ).first().addClass( "active" );
+
+
+
+function openbox(id,tt) {
+              var div = document.getElementById(id);
+              var tt_div = document.getElementById(tt);
+              if(div.style.display == 'block') {
+                div.style.display = 'none';
+              }
+              else {
+                div.style.display = 'block';
+              }
+              }
